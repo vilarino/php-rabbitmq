df9a6ece1c6d1f39a245585ad03f69dd8e558df6
Rafael Vilarino <rovilarino@gmail.com>
2023-10-18 17:42:34 -0300

    created params annotation

--- src/Infra/Logger/NullLogger.php
+++ src/Infra/Logger/NullLogger.php
@@ -4,6 +4,12 @@ namespace App\Infra\Logger;
 
 class NullLogger extends AbstractLogger
 {
+    /**
+     * @param string $level
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
     public function log(string $level, string $message, array $context = [])
     {
         //var_dump($level, $message, $context); exit;
